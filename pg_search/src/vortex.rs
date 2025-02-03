2009e42a4f582212c12eaf350c131754f6ebc599
Adam Gutglick <adam@spiraldb.com>
2025-02-03 14:43:47 +0000

    .

--- pg_search/src/vortex.rs
+++ pg_search/src/vortex.rs
@@ -5,16 +5,15 @@ use std::{io::Write, sync::Arc};
 use async_trait::async_trait;
 use bytes::Bytes;
 use futures::stream::BoxStream;
-use futures::StreamExt;
+use futures::{FutureExt, StreamExt};
 use object_store::{
     path::Path, GetOptions, GetResult, GetResultPayload, ListResult, MultipartUpload, ObjectMeta,
     ObjectStore, PutMultipartOpts, PutOptions, PutPayload, PutResult,
 };
-use object_store::{GetRange, PutMode};
+use object_store::{GetRange, PutMode, UploadPart};
 use pgrx::pg_sys::Oid;
 use rustc_hash::FxHashMap;
-use tokio::runtime::Runtime;
-use tokio::sync::Mutex;
+use std::sync::{Mutex, RwLock};
 use vortex::io::{VortexReadAt, VortexWrite};
 
 use crate::postgres::storage::block::FileEntry;
@@ -22,9 +21,18 @@ use crate::postgres::storage::{linked_bytes::RangeData, LinkedBytesList};
 
 #[derive(Debug)]
 struct BlockObjectStore {
-    inner: Arc<Mutex<FxHashMap<Path, FileEntry>>>,
+    fs_map: Arc<RwLock<FxHashMap<Path, FileEntry>>>,
     // I just assume that's available from somewhere, I don't fully understand the index oid lifecycle yet
     relation_oid: Oid,
+}
+
+impl BlockObjectStore {
+    fn new(relation_oid: Oid) -> Self {
+        Self {
+            relation_oid,
+            fs_map: Default::default(),
+        }
+    }
 }
 
 impl std::fmt::Display for BlockObjectStore {
@@ -92,6 +100,91 @@ impl BlockWriter {
     }
 }
 
+#[derive(Debug)]
+struct BlockMultipartWrite {
+    inner: Option<Arc<Mutex<Vec<u8>>>>,
+    fs_map: Arc<RwLock<FxHashMap<Path, FileEntry>>>,
+    relation_oid: Oid,
+    location: Path,
+}
+
+impl BlockMultipartWrite {
+    fn new(
+        relation_oid: Oid,
+        location: Path,
+        fs_map: Arc<RwLock<FxHashMap<Path, FileEntry>>>,
+    ) -> Self {
+        Self {
+            inner: Some(Default::default()),
+            fs_map,
+            relation_oid,
+            location,
+        }
+    }
+}
+
+#[async_trait]
+impl MultipartUpload for BlockMultipartWrite {
+    fn put_part(&mut self, data: PutPayload) -> UploadPart {
+        match self.inner.as_ref() {
+            None => unreachable!(),
+            Some(inner) => {
+                let lock = inner.clone();
+                async move {
+                    let mut guard = lock.lock().unwrap();
+                    let v: &mut Vec<u8> = guard.as_mut();
+                    for b in data.iter() {
+                        std::io::Write::write(v, b.as_ref()).unwrap();
+                    }
+
+                    Ok(())
+                }
+                .boxed()
+            }
+        }
+    }
+
+    async fn complete(&mut self) -> object_store::Result<PutResult> {
+        let data = self.inner.take().unwrap();
+        let guard = data.lock().unwrap();
+
+        // I think this is fine as we assume that every `put` creates a new file
+        let mut linked_bytes_list = unsafe { LinkedBytesList::create(self.relation_oid) };
+
+        linked_bytes_list
+            .write_all(guard.as_ref())
+            .map_err(|source| object_store::Error::Generic {
+                store: "BlockObjectStore",
+                source: Box::new(source),
+            })?;
+
+        linked_bytes_list
+            .flush()
+            .map_err(|source| object_store::Error::Generic {
+                store: "BlockObjectStore",
+                source: Box::new(source),
+            })?;
+
+        self.fs_map.write().unwrap().insert(
+            self.location.clone(),
+            FileEntry {
+                staring_block: linked_bytes_list.header_blockno,
+                total_bytes: guard.len(),
+            },
+        );
+
+        Ok(PutResult {
+            e_tag: None,
+            version: None,
+        })
+    }
+
+    async fn abort(&mut self) -> object_store::Result<()> {
+        self.inner.take().unwrap();
+        Ok(())
+    }
+}
+
 impl VortexWrite for BlockWriter {
     fn write_all<B: vortex::io::IoBuf>(
         &mut self,
@@ -148,7 +241,7 @@ impl ObjectStore for BlockObjectStore {
                 source: Box::new(source),
             })?;
 
-        self.inner.lock().await.insert(
+        self.fs_map.write().unwrap().insert(
             location.clone(),
             FileEntry {
                 staring_block: linked_bytes_list.header_blockno,
@@ -164,10 +257,14 @@ impl ObjectStore for BlockObjectStore {
 
     async fn put_multipart_opts(
         &self,
-        _location: &Path,
+        location: &Path,
         _opts: PutMultipartOpts,
     ) -> object_store::Result<Box<dyn MultipartUpload>> {
-        unimplemented!()
+        Ok(Box::new(BlockMultipartWrite::new(
+            self.relation_oid,
+            location.clone(),
+            self.fs_map.clone(),
+        )))
     }
 
     async fn get_opts(
@@ -175,7 +272,7 @@ impl ObjectStore for BlockObjectStore {
         location: &Path,
         options: GetOptions,
     ) -> object_store::Result<GetResult> {
-        let entry = self.inner.lock().await.get(location).unwrap().clone();
+        let entry = self.fs_map.read().unwrap().get(location).unwrap().clone();
         let linked_bytes_list = LinkedBytesList::open(self.relation_oid, entry.staring_block);
 
         let range = match options.range {
@@ -214,7 +311,7 @@ impl ObjectStore for BlockObjectStore {
     }
 
     async fn delete(&self, location: &Path) -> object_store::Result<()> {
-        let mut guard = self.inner.lock().await;
+        let mut guard = self.fs_map.write().unwrap();
         let entry = guard.get(location).unwrap();
         let mut linked_bytes_list = LinkedBytesList::open(self.relation_oid, entry.staring_block);
         unsafe { linked_bytes_list.mark_deleted() };
@@ -223,7 +320,7 @@ impl ObjectStore for BlockObjectStore {
     }
 
     fn list(&self, prefix: Option<&Path>) -> BoxStream<'_, object_store::Result<ObjectMeta>> {
-        let map = self.inner.blocking_lock().clone();
+        let map = self.fs_map.read().unwrap().clone();
         let prefix = prefix.cloned().unwrap_or_default();
 
         Box::pin(futures::stream::iter(map.into_iter().filter_map(
@@ -247,7 +344,7 @@ impl ObjectStore for BlockObjectStore {
 
     async fn list_with_delimiter(&self, prefix: Option<&Path>) -> object_store::Result<ListResult> {
         let prefix = prefix.cloned().unwrap_or_default();
-        let map = self.inner.blocking_lock();
+        let map = self.fs_map.read().unwrap();
 
         let mut common_prefixes = BTreeSet::new();
         let mut objects = vec![];
@@ -297,16 +394,23 @@ impl ObjectStore for BlockObjectStore {
 #[pgrx::pg_schema]
 mod tests {
     use datafusion::{
-        execution::{object_store::ObjectStoreUrl, SessionState, SessionStateBuilder},
+        common::GetExt,
+        datasource::provider::DefaultTableFactory,
+        execution::{object_store::ObjectStoreUrl, SessionStateBuilder},
         prelude::SessionContext,
     };
     use pgrx::{pg_sys, pg_test, Spi};
     use vortex::{
-        array::PrimitiveArray,
+        array::{BoolArray, PrimitiveArray, StructArray, VarBinArray},
+        buffer::buffer,
+        dtype::{DType, FieldNames, Nullability},
         file::{ExecutionMode, Scan, VortexOpenOptions, VortexWriteOptions},
+        io::ObjectStoreWriter,
         sampling_compressor::ALL_ENCODINGS_CONTEXT,
+        validity::Validity,
         IntoArray,
     };
+    use vortex_datafusion::persistent::VortexFormatFactory;
 
     use super::*;
 
@@ -366,30 +470,69 @@ mod tests {
         tokio::runtime::Builder::new_current_thread()
             .build()?
             .block_on(async move {
-                let object_store = Arc::new(BlockObjectStore {
-                    inner: Default::default(),
-                    relation_oid,
-                });
-
+                // A lot of datafusion-specific boilerplate to setup vortex and storage
+                let object_store = Arc::new(BlockObjectStore::new(relation_oid));
                 let url = ObjectStoreUrl::parse("file://")?;
 
-                // Missing a piece that will be in tomorrow's (January 31st) Vortex release
-                // let state = SessionStateBuilder::new().with_file_formats(file_formats)
-                let session = SessionContext::new();
-                _ = session.register_object_store(url.as_ref(), object_store);
+                let table_path = Path::parse("/path/to/data/")?;
 
-                let _df = session
-                    .sql(
-                        "CREATE EXTERNAL TABLE tbl
-                    STORED AS VORTEX
-                    LOCATION '/path/to/data';",
-                    )
+                let xs = PrimitiveArray::new(buffer![0i64, 1, 2, 3, 4], Validity::NonNullable);
+                let ys = VarBinArray::from_vec(
+                    vec!["a", "b", "c", "d", "e"],
+                    DType::Utf8(Nullability::NonNullable),
+                );
+                let zs = BoolArray::from_iter([true, true, true, false, false]);
+
+                let table_data = StructArray::try_new(
+                    FieldNames::from(["xs".into(), "ys".into(), "zs".into()]),
+                    vec![xs.into_array(), ys.into_array(), zs.into_array()],
+                    5,
+                    Validity::NonNullable,
+                )?
+                .into_array();
+
+                let object_writer =
+                    ObjectStoreWriter::new(object_store.clone(), table_path.child("data.vortex"))
+                        .await?;
+
+                let mut w = VortexWriteOptions::default()
+                    .write(object_writer, table_data.into_array_stream())
                     .await?;
 
-                // TODO: insert some data
-                // TODO: hopefully query it back
+                w.flush().await?;
+
+                let factory = VortexFormatFactory::default_config();
+                let mut session_state_builder = SessionStateBuilder::new().with_default_features();
+
+                if let Some(table_factories) = session_state_builder.table_factories() {
+                    table_factories.insert(
+                        GetExt::get_ext(&factory).to_uppercase(), // Has to be uppercase
+                        Arc::new(DefaultTableFactory::new()),
+                    );
+                }
+
+                if let Some(file_formats) = session_state_builder.file_formats() {
+                    file_formats.push(Arc::new(factory));
+                }
+
+                let session = SessionContext::new_with_state(session_state_builder.build());
+                _ = session.register_object_store(url.as_ref(), object_store);
+
+                session
+                    .sql(
+                        "CREATE EXTERNAL TABLE tbl
+                        STORED AS vortex
+                        LOCATION '/path/to/data/';",
+                    )
+                    .await?
+                    .collect()
+                    .await?;
+
+                session.table("tbl").await?.show().await?;
 
                 anyhow::Ok(())
-            })
+            })?;
+
+        anyhow::Ok(())
     }
 }
